e89073a7adf6d6fa975763465b15c7c560e21943
danipoav <dapoav2002@gmail.com>
2025-02-03 13:29:19 +0100

    creating Arrays

--- index1.php
+++ index1.php
@@ -1,4 +1,28 @@
 <?php
+$room1 = [
+    'id' => '1',
+    'name' => 'Daniel',
+    'number' => '10',
+    'price' => '200',
+    'discount' => '10'
+];
 
-echo 'Hola'
-?>
+$room2 = [
+    'id' => '2',
+    'name' => 'Ariana',
+    'number' => '20',
+    'price' => '400',
+    'discount' => '20'
+];
+
+$room3 = [
+    'id' => '1',
+    'name' => 'Nelly',
+    'number' => '30',
+    'price' => '600',
+    'discount' => '30'
+];
+
+echo "<pre>";
+print_r([$room1, $room2, $room3]);
+echo "</pre>";
