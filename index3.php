b73d142729747a0440e5350d44fc322f6d3cb6e1
danipoav <dapoav2002@gmail.com>
2025-02-03 14:16:20 +0100

    Printing each room

--- index3.php
+++ index3.php
@@ -1,4 +1,22 @@
 <?php
+$jsonRoot = 'rooms.json';
+$jsonGetData = file_get_contents($jsonRoot);
+$roomsArray = json_decode($jsonGetData, true);
 
-echo 'Hola'
-?>
+if ($roomsArray === null) {
+    echo "Error al decodificar";
+    exit;
+}
+
+foreach ($roomsArray as $index => $room) {
+    echo '<pre>';
+    echo "<p><strong>Índice:</strong> $index</p>";
+    echo '<ol>';
+    echo '<li> ID: ' . $room['id'] . '</li>';
+    echo '<li> PRICE: ' . $room['price'] . '</li>';
+    echo '<li> NAME: ' . $room['name'] . '</li>';
+    echo '<li> BED TYPE: ' . $room['bed_type'] . '</li>';
+    echo '<li> STATUS: ' . $room['status'] . '</li>';
+    echo '</ol>';
+    echo '</pre>';
+}
