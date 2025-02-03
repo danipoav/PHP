056ef9e2cfdfbf6415be9d57cf1f5a6df081dd1f
danipoav <dapoav2002@gmail.com>
2025-02-03 14:00:10 +0100

    Printing json files

--- index2.php
+++ index2.php
@@ -1,3 +1,13 @@
 <?php
+$jsonFile = 'rooms.json';
+$jsonGetData = file_get_contents($jsonFile);
+$rooms = json_decode($jsonGetData, true);
 
-?>
+if ($rooms === null) {
+    echo "Error al decodificar";
+    exit;
+}
+
+echo "<pre>";
+print_r($rooms);
+echo "</pre>";
