24cc5c60a2ae8428230f477cd16685a95c15f43f
danipoav <dapoav2002@gmail.com>
2025-02-04 17:17:07 +0100

    Getting rooms from my dtbase

--- index5.php
+++ index5.php
@@ -1,4 +1,21 @@
 <?php
+$conn = new mysqli('localhost', 'Dani', 'Batman2002', 'db_hotel');
 
-echo 'Hola'
-?>
+if ($conn->connect_errno) {
+    echo "Failed to connect to MySQL: " . $conn->connect_error;
+    exit();
+}
+
+$sql = "SELECT * FROM rooms";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo '<pre>';
+    while ($row = $result->fetch_assoc()) {
+        print_r($row);
+        echo "\n";
+    }
+    echo '</pre>';
+} else {
+    echo "No hay habitaciones disponibles.";
+}
