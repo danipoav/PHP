9a78a20ba2db1550d970dd2ad689c5e37e75d549
danipoav <dapoav2002@gmail.com>
2025-02-04 15:26:12 +0100

    Pasing params to index4.php tofound the right room

--- index4.php
+++ index4.php
@@ -1,4 +1,35 @@
 <?php
+if (!isset($_GET['id'])) {
+    echo 'Error no params recived';
+    exit;
+}
 
-echo 'Hola'
-?>
+$id = $_GET['id'];
+
+$jsonRoot = 'rooms.json';
+$jsonGetData = file_get_contents($jsonRoot);
+$roomsArray = json_decode($jsonGetData, true);
+
+$roomResult = null;
+
+foreach ($roomsArray as $room) {
+    if ($room['id'] === $id) {
+        $roomResult = $room;
+        break;
+    }
+}
+
+if (!$roomResult) {
+    echo 'No se encontró ninguna habitación con ID: ' . $id;
+    exit;
+}
+
+echo '<pre>';
+echo '<ul>';
+echo '<li> ID: ' . $roomResult['id'] . '</li>';
+echo '<li> PRICE: ' . $roomResult['price'] . '</li>';
+echo '<li> NAME: ' . $roomResult['name'] . '</li>';
+echo '<li> BED TYPE: ' . $roomResult['bed_type'] . '</li>';
+echo '<li> STATUS: ' . $roomResult['status'] . '</li>';
+echo '</ul>';
+echo '</pre>';
